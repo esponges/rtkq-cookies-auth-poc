9d8c4ded857d9bb626ab4ba78f14efae78c780b0
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-09-03 14:27:41 -0600

    make token a timestamp

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -7,15 +7,39 @@ export type LoginResponse = {
   userName: string;
 };
 
+// generate a time stamp for the token
+const generateTimeStamp = (offset?: number) => {
+  const now = new Date();
+  const newDate = new Date(now.getTime() + (offset ?? 1 * 1000));
+  // to ISO string
+  const iso = newDate.toISOString();
+
+  return iso;
+}
+
+// receives ISO and increase it by offset - returns ISO
+const increaseTimeStamp = (timeStamp: string, offset?: number) => {
+  const date = new Date(timeStamp);
+  const newDate = new Date(date.getTime() + (offset ?? 1 * 1000));
+  // to ISO string
+  const iso = newDate.toISOString();
+
+  return iso;
+}
+
 // set a resolved promise that gets a new token and refreshToken
 const loginMutation = async () => {
+  const token = generateTimeStamp();
+  // refresh token should be 5 more days
+  const refreshToken = increaseTimeStamp(token, 5 * 24 * 60 * 60 * 1000);
+
   const response = await new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        token: 'token',
-        refreshToken: 'refreshToken',
-        userEmail: 'userEmail',
-        userName: 'userName',
+        token,
+        refreshToken,
+        userEmail: 'foo@boo.com',
+        userName: 'foowie booie',
       });
     }, 1000);
   }) as LoginResponse;
