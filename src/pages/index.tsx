c71ce9cd729bf26b6af9be918066c5620258b725
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-10-05 18:35:10 -0600

    logout feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import { useRouter } from 'next/router';
 
 import { useLoginMutation } from '@/store/services/auth';
 import { AUTH_REFRESH_TOKEN, AUTH_TOKEN, removeCookies } from '@/lib/cookies';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store';
+import { logout } from '@/store/slices/auth';
 
 export default function Home() {
   const router = useRouter();
+  const dispatch = useDispatch();
   const [signIn] = useLoginMutation();
+  const { userEmail } = useSelector((state: RootState) => state.auth);
 
   const handleLogin = async () => {
     try {
@@ -20,9 +25,13 @@ export default function Home() {
     }
   };
 
+  const handleLogout = () => {
+    dispatch(logout());
+  }
+
   return (
     <div className='text-center mt-10'>
-      <h1 className='text-xl mb-5'>Welcome to the login</h1>
+      <h1 className='text-xl mb-5'>Welcome {userEmail || 'Guest'}</h1>
       <div className='mb-5 flex justify-center'>
         <Link
           href='/authed'
@@ -30,12 +39,21 @@ export default function Home() {
         >
           <span className='text-xl'>Go to authed route</span>
         </Link>
-        <button
-          onClick={handleLogin}
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-5 rounded'
-        >
-          <span className='text-xl'>Login</span>
-        </button>
+        {!userEmail ? (
+          <button
+            onClick={handleLogin}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-5 rounded'
+          >
+            <span className='text-xl'>Login</span>
+          </button>
+        ) : (
+          <button
+            onClick={handleLogout}
+            className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 ml-5 rounded'
+          >
+            <span className='text-xl'>Logout</span>
+          </button>
+        )}
         {/* remove cookies for testing */}
         <button
           onClick={() => removeCookies([AUTH_TOKEN, AUTH_REFRESH_TOKEN])}
--- src/store/slices/auth.ts
+++ src/store/slices/auth.ts
@@ -1,7 +1,7 @@
 import { LoginResponse } from '@/pages/api/login';
 import { createSlice } from '@reduxjs/toolkit';
 import { authApi } from '../services/auth';
-import { AUTH_REFRESH_TOKEN, AUTH_TOKEN, setAuthCookie } from '@/lib/cookies';
+import { AUTH_REFRESH_TOKEN, AUTH_TOKEN, removeCookies, setAuthCookie } from '@/lib/cookies';
 import { userApi } from '../services/user';
 
 const initialState: Partial<LoginResponse> = {};
@@ -9,28 +9,36 @@ const initialState: Partial<LoginResponse> = {};
 const slice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  reducers: {
+    // logout
+    logout: () => {
+      // remove the token and refreshToken
+      removeCookies([AUTH_TOKEN, AUTH_REFRESH_TOKEN]);
+      return initialState;
+    },
+  },
   extraReducers: (builder) => {
     builder
-    .addMatcher(
-      authApi.endpoints.login.matchFulfilled,
-      (_state, { payload }) => {
-        // set the token and refreshToken
-        setAuthCookie(payload.token, AUTH_TOKEN);
-        setAuthCookie(payload.refreshToken, AUTH_REFRESH_TOKEN);
+      .addMatcher(
+        authApi.endpoints.login.matchFulfilled,
+        (_state, { payload }) => {
+          // set the token and refreshToken
+          setAuthCookie(payload.token, AUTH_TOKEN);
+          setAuthCookie(payload.refreshToken, AUTH_REFRESH_TOKEN);
 
-        return payload;
-      }
-    )
-    .addMatcher(
-      userApi.endpoints.userDetails.matchFulfilled,
-      (_state, { payload }) => {
-        // optional:
-        // depending on the api response we could also set cookies again here
-        return payload;
-      }
-    );
+          return payload;
+        }
+      )
+      .addMatcher(
+        userApi.endpoints.userDetails.matchFulfilled,
+        (_state, { payload }) => {
+          // optional:
+          // depending on the api response we could also set cookies again here
+          return payload;
+        }
+      );
   },
 });
 
 export const authReducer = slice.reducer;
+export const { logout } = slice.actions;
